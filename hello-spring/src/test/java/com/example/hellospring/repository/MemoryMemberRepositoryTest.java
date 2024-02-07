aec6695a073fc6ea86e0778b5cedbd170ab6cee2
cylcoder <cylcoder@gmail.com>
2024-02-07 11:07:34 +0900

    first commit

---
+++ hello-spring/src/test/java/com/example/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,50 @@
+package com.example.hellospring.repository;
+
+import com.example.hellospring.domain.Member;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MemoryMemberRepositoryTest {
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach() {
+        repository.clearStore();
+    }
+
+    @Test
+    public void save() {
+        Member member = new Member();
+        member.setName("Spring");
+        repository.save(member);
+        Member result = repository.findById(member.getId()).get();
+        assertThat(member).isEqualTo(result);
+    }
+
+    @Test
+    public void findByName() {
+        Member member = new Member();
+        member.setName("Django");
+        repository.save(member);
+        Member result = repository.findByName("Django").get();
+        assertThat(member).isEqualTo(result);
+    }
+
+    @Test
+    public void findAll() {
+        Member member1 = new Member();
+        member1.setName("Django");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("Spring");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+        assertThat(result.size()).isEqualTo(2);
+    }
+}
